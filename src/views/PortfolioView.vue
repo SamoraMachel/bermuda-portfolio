b0806fbc72251591bb0bd979920084fd5351c798
Samora Machel <samora@work.laptop>
2023-07-16 01:52:32 +0300

    Added a horizontal scroll for the application

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,6 +1,9 @@
 <script setup>
 import HeroSection from '../components/HeroSection.vue'
 import HomeSection from '../components/HomeSection.vue'
+import FeatureSection from '../components/FeatureSection.vue';  
+import TeamSection from '../components/TeamSection.vue';    
+import ContactSection from '../components/ContactSection.vue';
 </script>
 
 <template>
@@ -8,6 +11,9 @@ import HomeSection from '../components/HomeSection.vue'
         <HeroSection>
             <template #content>
                 <HomeSection />
+                <FeatureSection />
+                <TeamSection />
+                <ContactSection />
             </template>
         </HeroSection>
     </div>
