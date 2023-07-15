38af7f08687075f1c493358afaed0613e86b41c1
Samora Machel <samora@work.laptop>
2023-07-16 02:00:55 +0300

    Changed the navigation from a tag to internal vue router-link

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -3,7 +3,7 @@
     <header class="absolute inset-x-0 top-0 z-50">
     <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div class="flex lg:flex-1">
-        <a href="#" class="-m-1.5 p-1.5">
+        <a href="/" class="-m-1.5 p-1.5">
             <span class="font-bold tracking-widest">Bermuda</span>
             <!-- <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" /> -->
         </a>
@@ -37,7 +37,7 @@
         <div class="mt-6 flow-root">
             <div class="-my-6 divide-y divide-gray-500/10">
             <div class="space-y-2 py-6">
-                <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{{ item.name }}</a>
+                <router-link v-for="item in navigation" :key="item.name" to="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{{ item.name }}</router-link>
             </div>
             
             </div>
